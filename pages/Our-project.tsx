9376279b31bcc1f2b15320681517adcfbe66db70
dungtri2803 <nguyentridung2803@gmail.com>
2022-08-19 11:46:16 +0700

    rename

--- pages/Our-project.tsx
+++ pages/Our-project.tsx
@@ -1,22 +1,9 @@
 import type { NextPage } from "next";
-import Image from "next/image";
-import React, { useState, useEffect } from "react";
 import Head from "next/head";
-import Ourservice from "../components/Ourservice/Ourservice";
-import Career from "../components/Career/Career";
-import Ourproject from "../components/Ourproject/Ourproject";
-import About from "../components/About/About";
-import Testimonial from "../components/Testimonial/Testimonial";
-import Brand from "../components/Brand/Brand";
-import Contact from "../components/Contact/Contact";
-import Footer from "../components/Footer/Footer";
-import Loading from "../components/Loading/Loading";
-import OurprojectMobile from "../components/Ourproject/OurprojectMobile";
-import OurteamDetail from "../components/OurteamDetail/OurteamDetail";
+import { useEffect, useState } from "react";
 import OurprojectDetail from "../components/OurprojectDetail/OurprojectDetail";
-import Homes from "../components/Home/Home";
 
-const Ourteam: NextPage = () => {
+const Ourproject: NextPage = () => {
   const [LoadingPage, setLoadingPage] = useState(true);
   useEffect(() => {
     setTimeout(() => {
@@ -33,4 +20,4 @@ const Ourteam: NextPage = () => {
   );
 };
 
-export default Ourteam;
+export default Ourproject;
