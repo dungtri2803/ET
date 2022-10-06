96a6a85af590d00dda31d4b4a57b7c61c3d98fd7
dungtri2803 <nguyentridung2803@gmail.com>
2022-10-06 11:38:53 +0700

    up

--- components/Modal/ModalCareer.tsx
+++ components/Modal/ModalCareer.tsx
@@ -17,16 +17,7 @@ function ModalCareer({ CloseModal, CloseE }: any) {
     headers: {
       authorization: "authorization-text",
     },
-    onChange(info) {
-      if (info.file.status !== "uploading") {
-        console.log(info.file, info.fileList);
-      }
-      if (info.file.status === "done") {
-        message.success(`${info.file.name} file uploaded successfully`);
-      } else if (info.file.status === "error") {
-        message.error(`${info.file.name} file upload failed.`);
-      }
-    },
+    
     progress: {
       strokeColor: {
         "0%": "#1CBED2",
